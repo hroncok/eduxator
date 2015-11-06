f911bf7f7fadb2be28c19c037e272b7b297a6e0a
Miro Hrončok <miro@hroncok.cz>
2015-11-06 15:05:23 +0100

    Also POST data

--- eduxator/io.py
+++ eduxator/io.py
@@ -1,18 +1,19 @@
 import os
 
-import lxml
+from lxml import etree
 import requests
 
 
 COOKIE = '~/.edux.cookie'
-FORMURL = 'https://edux.fit.cvut.cz/courses/BI-3DT/classification/view/fulltime/tutorials/3?do=edit'
+POST = 'https://edux.fit.cvut.cz/courses/BI-3DT/classification/view/fulltime/tutorials/3'
+GET = POST + '?do=edit'
 
 
 class EduxIO:
 
     '''Class providing the interface for reading and writing Edux classification'''
 
-    def __init__(self, cookie_file=None, cookie_dict=None):
+    def __init__(self, *, cookie_file=None, cookie_dict=None):
         if cookie_file and cookie_dict:
             raise ValueError('cookie_file and cookie_dict cannot be used at teh same time')
         if cookie_dict:
@@ -28,12 +29,16 @@ class EduxIO:
     @classmethod
     def _cookie_from_file(cls, path):
         with open(os.path.expanduser(path)) as f:
-            first_line = f.readlines()[0]
-        parts = first_line.split('=')
-        return {parts[0]: parts[1]}
+            lines = f.readlines()
+        cookies = {}
+        for line in lines:
+            parts = line.split('=')
+            cookies[parts[0]] = parts[1].rstrip()
+        return cookies
 
     def get(self, url):
         r = requests.get(url, cookies=self.cookies)
+        self.cookies = r.cookies.get_dict()
 
         # Hear about return codes, Edux?
         if ('id="nepovolena_akce"' in r.text or
@@ -46,9 +51,12 @@ class EduxIO:
 
         return r
 
-    def parse_form_edit_score(self, url=FORMURL):
+    def post(self, url, data):
+        return requests.post(url, data, cookies=self.cookies)
+
+    def parse_form_edit_score(self, url=GET):
         r = self.get(url)
-        tree = lxml.etree.HTML(r.text)
+        tree = etree.HTML(r.text)
         scores_form = None
         for form in tree.findall('.//form'):
             if form.attrib.get('id') in ['cs_form_edit_score', 'en_form_edit_score']:
@@ -62,3 +70,6 @@ class EduxIO:
             values[inp.attrib.get('name')] = inp.attrib.get('value', '')
         values.pop(None, None)  # Remove bogus value, such as 'nastavit svislý posun'
         return values
+
+    def submit_form_edit_score(self, data, url=POST):
+        return self.post(url, data)

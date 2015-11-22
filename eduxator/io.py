09d630625e53abce82a3290e79e97859c4c332c3
Miro Hrončok <miro@hroncok.cz>
2015-11-22 14:24:07 +0100

    Remove unused API parts

--- eduxator/io.py
+++ eduxator/io.py
@@ -71,23 +71,20 @@ class EduxIO:
         return tuple(x[len(COURSES):] for x in set(re.findall(COURSES + r'[^<"]*', r.text))
                      if not x.endswith('KOD-PREDMETU'))
 
-    def parse_classification_tree(self, course=None):
+    def parse_classification_tree(self):
         '''
         Parse all classification types for our course
 
-        If course is not provided, tries to use self.course
-        If course is provided and self.course was not set, it sets it
-        If course is provided and self.course was set, it uses course and preserves self.course
-        If neither course or self.course is provided/set, it blows up
+        If self.course is not set, it blows up
 
         Returns a weird dict-based tree where leaves contains empty dicts
         '''
-        course = self.use_course(course)
+        self.check_attr('course')
 
-        classification = EDUX + COURSES + course + CLASSIFICATION
+        classification = EDUX + COURSES + self.course + CLASSIFICATION
         r = self.get(classification + 'start')
         # link is HTML are without EDUX address
-        classification = '/' + COURSES + course + CLASSIFICATION
+        classification = '/' + COURSES + self.course + CLASSIFICATION
         strings = tuple(x[len(classification):] for x in
                         set(re.findall(classification + r'[^ <"\?#]*', r.text))
                         if not x.startswith(classification + 'start') and not x.endswith('start')
@@ -102,40 +99,27 @@ class EduxIO:
                 walk = walk[part]
         return tree
 
-    def use_course(self, course):
-        if hasattr(self, 'course'):
-            return course or self.course
-        elif course is None:
-            raise AttributeError('Neither course attribute or course argument was provided')
-        self.course = course
-        return course
+    def check_attr(self, attribute):
+        if not hasattr(self, attribute):
+            raise AttributeError(attribute + ' attribute was not provided, cannot continue')
 
-    def use_classpath(self, classpath):
-        if hasattr(self, 'classpath'):
-            return classpath or self.classpath
-        elif classpath is None:
-            raise AttributeError('Neither classpath attribute or classpath argument was provided')
-        self.classpath = classpath
-        return classpath
-
-    def construct_form_url(self, course, classpath, *, edit=False):
-        course = self.use_course(course)
-        classpath = self.use_classpath(classpath)
+    def construct_form_url(self, *, edit=False):
+        self.check_attr('course')
+        self.check_attr('classpath')
         extra = EDIT if edit else ''
-        return EDUX + COURSES + course + CLASSIFICATION + '/'.join(classpath) + extra
+        return EDUX + COURSES + self.course + CLASSIFICATION + '/'.join(self.classpath) + extra
 
-    def parse_form_edit_score(self, *, course=None, classpath=None):
+    def parse_form_edit_score(self):
         '''
         Parse the classification form
 
-        course behaves the same as in parse_classification_tree()
-        classpath behaves the same as course
+        course and classpath attributes have to be set
 
         classpath is supposed to be list of strings
 
         Returns a dict of all current values
         '''
-        url = self.construct_form_url(course, classpath, edit=True)
+        url = self.construct_form_url(edit=True)
         r = self.get(url)
         tree = etree.HTML(r.text)
         scores_form = None
@@ -152,6 +136,6 @@ class EduxIO:
         values.pop(None, None)  # Remove bogus value, such as 'nastavit svislý posun'
         return values
 
-    def submit_form_edit_score(self, data, *, course=None, classpath=None):
-        url = self.construct_form_url(course, classpath)
+    def submit_form_edit_score(self, data):
+        url = self.construct_form_url()
         return self.post(url, data)
--- test/test_io.py
+++ test/test_io.py
@@ -19,55 +19,35 @@ class TestEduxIO():
             text = f.read()
         return flexmock(text=text, cookies=flexmock(get_dict=lambda: {}))
 
-    @pytest.mark.parametrize('argument', (True, False))
-    def test_parsing_form(self, argument):
+    def test_parsing_form(self):
         url = 'https://edux.fit.cvut.cz/courses/BI-3DT/' + \
               'classification/view/fulltime/tutorials/3?do=edit'
         flexmock(io.EduxIO).should_receive('get').with_args(url).once().and_return(
             self.fake_response('form'))
         e = io.EduxIO(cookie_dict={})
-        if argument:
-            data = e.parse_form_edit_score(course='BI-3DT',
-                                           classpath=('fulltime', 'tutorials', '3'))
-        else:
-            e.course = 'BI-3DT'
-            e.classpath = ('fulltime', 'tutorials', '3')
-            data = e.parse_form_edit_score()
+        e.course = 'BI-3DT'
+        e.classpath = ('fulltime', 'tutorials', '3')
+        data = e.parse_form_edit_score()
         for key, value in self.good_data('form').items():
             assert data[key] == value
-        assert e.course == 'BI-3DT'
-        assert e.classpath == ('fulltime', 'tutorials', '3')
 
-    @pytest.mark.parametrize('argument', (True, False))
-    def test_sending_form(self, argument):
+    def test_sending_form(self):
         url = 'https://edux.fit.cvut.cz/courses/BI-3DT/classification/view/fulltime/tutorials/3'
         flexmock(io.EduxIO).should_receive('post').with_args(url, {}).once()
         e = io.EduxIO(cookie_dict={})
-        if argument:
-            e.submit_form_edit_score({}, course='BI-3DT',
-                                     classpath=('fulltime', 'tutorials', '3'))
-        else:
-            e.course = 'BI-3DT'
-            e.classpath = ('fulltime', 'tutorials', '3')
-            e.submit_form_edit_score({})
-        assert e.course == 'BI-3DT'
-        assert e.classpath == ('fulltime', 'tutorials', '3')
+        e.course = 'BI-3DT'
+        e.classpath = ('fulltime', 'tutorials', '3')
+        e.submit_form_edit_score({})
 
     def test_parsing_courses(self):
         flexmock(requests).should_receive('get').once().and_return(self.fake_response('courses'))
         e = io.EduxIO(cookie_dict={})
         assert sorted(self.good_data('courses')) == sorted(e.parse_courses_list())
 
-    @pytest.mark.parametrize('argument', (True, False))
-    def test_parsing_calssification(self, argument):
+    def test_parsing_calssification(self):
         url = 'https://edux.fit.cvut.cz/courses/BI-3DT/classification/view/start'
         flexmock(io.EduxIO).should_receive('get').with_args(url).once().and_return(
             self.fake_response('classification'))
         e = io.EduxIO(cookie_dict={})
-        if argument:
-            tree = e.parse_classification_tree('BI-3DT')
-        else:
-            e.course = 'BI-3DT'
-            tree = e.parse_classification_tree()
-        assert e.course == 'BI-3DT'
-        assert tree == self.good_data('classification')
+        e.course = 'BI-3DT'
+        tree = e.parse_classification_tree()

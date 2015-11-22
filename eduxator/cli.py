2e3aa510839d546c612128f8fa58d64d7ab74283
Miro Hrončok <miro@hroncok.cz>
2015-11-22 02:13:48 +0100

    Determine context

--- eduxator/cli.py
+++ eduxator/cli.py
@@ -8,6 +8,7 @@ class CLI:
     def __init__(self):
         self.prompt = '> '
         self.cookie_setup()
+        self.context_setup()
 
     def cookie_setup(self):
         try:
@@ -24,6 +25,62 @@ class CLI:
                              'to save you some pain later?'):
                 self.info('Saving.')
                 self.eduxio.save_cookie()
+
+    def context_setup(self):
+        args = list(set(sys.argv[1:]))
+        self.eduxio.course = self.determine_course(args)
+        self.eduxio.classpath = self.determine_classpath(args)
+
+    def find_candidates(self, args, possibilities, case_sentitive=False):
+        if len(possibilities) == 1:
+            return possibilities
+
+        candidates = []
+
+        lowers = possibilities if case_sentitive else [x.lower() for x in possibilities]
+        for i, arg in enumerate(args):
+            larg = arg if case_sentitive else arg.lower()
+            if larg in lowers:
+                if case_sentitive:
+                    # shortcut, just add the arg as it is
+                    candidates.append(arg)
+                else:
+                    # save the item as it is in the possibilities
+                    candidates.append(possibilities[lowers.index(larg)])
+
+        if len(candidates) == 1:
+            # we only remove the thing form args if we are sure we are going to use it
+            args.remove(candidates[0])
+
+        return candidates
+
+    def determine_course(self, args):
+        courses = self.eduxio.parse_courses_list()
+        candidates = self.find_candidates(args, courses)
+        if not candidates:
+            return self.ask('What course do you want?', courses)
+        if len(candidates) > 1:
+            self.warn('Found multiple possible courses.')
+            return self.ask('What course do you want?', candidates)
+        return candidates[0]
+
+    def determine_classpath(self, args):
+        tree = self.eduxio.parse_classification_tree()
+        if not tree:
+            raise ValueError('The classification tree is empty!')
+        classpath = []
+        while tree:
+            candidates = self.find_candidates(args, list(tree.keys()))
+            if not candidates:
+                one = self.ask('What class do you want?', tree.keys())
+            elif len(candidates) > 1:
+                self.warn('Found multiple possible classes.')
+                one = self.ask('What class do you want?', candidates)
+            else:
+                one = candidates[0]
+            classpath.append(one)
+            tree = tree[one]
+        return classpath
 
     def exit(self):
         sys.exit(0)
@@ -43,7 +100,7 @@ class CLI:
     def possibilities_list(self, possibilities):
         if possibilities:
             if len(possibilities) < 8:
-                return ' (' + '/'.join(possibilities) + '):'
+                return ' (' + '/'.join(sorted(possibilities)) + '):'
             return ' (use tab to help yourself):'
         return ''
 
